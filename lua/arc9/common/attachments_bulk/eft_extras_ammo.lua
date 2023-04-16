36c45e91610afbead0f19ee660b784e24f79995c
Darsu <95997959+dar-su@users.noreply.github.com>
2023-04-16 16:59:06 +0700

    eft_extras_ammo.lua

---
+++ lua/arc9/common/attachments_bulk/eft_extras_ammo.lua
@@ -0,0 +1,159 @@
+
+local dmgrange = (GetConVar("arc9_eft_mindmgrange"):GetInt() or 1000)/1000
+local mult39 = GetConVar("arc9_eft_mult_rifle"):GetFloat() or 0.5
+
+
+local ATT = {}
+
+///////////////////////////////////////      eft_ammo_12x70_explosive
+
+ATT = {}
+
+ATT.PrintName = "12/70 \"explosive\" slug"
+ATT.CompactName = "Explosive"
+ATT.Icon = Material("entities/eft_attachments/ammo/12x70/g40.png", "mips smooth")
+ATT.Description = [[Homemade 12/70 round for 12 gauge shotguns with an exlosive charge. The recipe was printed in a rare issue of the magazine "Homemade reloads", which was never published. Uses a cartridge case from a 12/70 "Poleva-6u" round.
+
+eft extras attachment]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+
+ATT.HasAmmoooooooo = true 
+
+ATT.EFTRoundName = "12/70 \"explosive\" slug"
+ATT.ShellModel = "models/weapons/arc9/darsu_eft/shells/patron_12x70_slug_grizzly_40_shell.mdl"
+ATT.ActivateElements = {"eft_ammo_12x70_grizzly40"}
+
+-- ATT.DamageMax = 51/2
+-- ATT.DamageMin = 20/2
+ATT.DamageMax = 55*5
+ATT.DamageMin = 55*5
+ATT.PhysBulletMuzzleVelocity = 251 /0.0254
+ATT.RangeMin = 10
+ATT.RangeMax = 1000 /0.0254
+
+ATT.Penetration =      16 *2.54/100/0.0254
+ATT.PenetrationDelta = 72/100
+ATT.ArmorPiercing =    72/100
+ATT.RicochetChance =   10/100
+
+ATT.Num = 1
+ATT.VisualRecoilMult = 1.25
+ATT.SpreadMult = 1.07
+
+ATT.MalfunctionMeanShotsToFailMult = 0.075
+
+
+ATT.ExplosionDamage = 55*5
+ATT.ExplosionRadius = 5
+ATT.ExplosionEffect = "eft_explosion_round"
+ATT.ImpactDecal = "FadingScorch"
+
+ATT.Override_DamageType = DMG_BLAST + DMG_AIRBOAT
+ATT.DamageType = DMG_BLAST + DMG_AIRBOAT
+
+ATT.Category = {"eft_ammo_12x70"}
+
+ARC9.LoadAttachment(ATT, "eft_ammo_12x70_explosive")
+
+
+
+///////////////////////////////////////      eft_ammo_76239_apiz
+
+
+ATT = {}
+
+ATT.PrintName = "7.62x39mm API Z"
+ATT.CompactName = "API Z"
+ATT.Icon = Material("entities/eft_attachments/ammo/762x39/hp.png", "mips smooth")
+ATT.Description = [[A 7.62x39mm API Z (GRAU Index - 57-3-231) cartridge, an armour piercing incendiary cartridge intended for use on lightly armoured vehicles. Extremely rare round that was discontinued in the early 1960s.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+
+ATT.HasAmmoooooooo = true 
+
+ATT.EFTRoundName = "7.62x39mm HP"
+
+ATT.DamageMax = 87 * mult39
+ATT.DamageMin = 54.4 * mult39
+ATT.PhysBulletMuzzleVelocity = 754 /0.0254
+-- ATT.TracerNum = 1
+-- ATT.TracerColor = Color(255, 16, 16)
+ATT.SpreadMult = 1.05
+-- ATT.RecoilMult = 1.05
+-- ATT.VisualRecoilMult = 1.05
+ATT.HeatPerShotMult = 1
+
+ATT.RangeMin = 10
+ATT.RangeMax = 1000 /0.0254 * dmgrange
+
+ATT.Penetration =      15 *2.54/100/0.0254
+ATT.PenetrationDelta = 35/100
+ATT.ArmorPiercing =    35/100
+ATT.RicochetChance =   18/100
+
+ATT.DamageLookupTable = {
+    {   10/0.0254 * dmgrange, 
+    87 * mult39     },
+
+    {   100 /0.0254 * dmgrange, 
+    80.85 * mult39     },
+
+    {   200 /0.0254 * dmgrange, 
+    75.16 * mult39     },
+
+    {   300 /0.0254 * dmgrange, 
+    68.78 * mult39     },
+
+    {   400 /0.0254 * dmgrange, 
+    64.81 * mult39     },
+
+    {   500 /0.0254 * dmgrange, 
+    62.66 * mult39     },
+
+    {   600 /0.0254 * dmgrange, 
+    61.13 * mult39     },
+
+    {   700 /0.0254 * dmgrange, 
+    60 * mult39     },
+
+    {   800 /0.0254 * dmgrange, 
+    59 * mult39     },
+
+    {   900 /0.0254 * dmgrange, 
+    58.17 * mult39     },
+
+    {   1000 /0.0254 * dmgrange, 
+    57.41 * mult39     },
+}
+
+-- 100m     200m    300m    400m    500m    600m    700m    800m    900m    1000m
+-- 80.85	75.16	68.78	64.81	62.66	61.13	59.97	59	58.17	57.41
+
+ATT.Category = {"eft_ammo_76239"}
+
+
+-- urban extras but arc9
+
+ATT.DamageTypeOverride = DMG_BURN + DMG_BULLET
+
+local cov = 1 -- ??
+
+local badblood = { -- it's actually the good type
+    [-1] = true,
+    [3] = true,
+}
+
+ATT.Hook_BulletImpact = function(wep,data)
+    local ent = data.tr.Entity
+    local test1 = !(ent:IsNPC() or ent:IsPlayer() or ent:IsNextBot()) and true or false
+    local test2 = (!ent:GetBloodColor() or badblood[ent:GetBloodColor()]) and true or false
+    if IsValid(ent) and (test1 or test2) then
+        data.dmg = data.dmg * cov:GetFloat()
+        local eff = EffectData()
+        eff:SetOrigin(data.tr.HitPos)
+        util.Effect("cball_bounce", eff)
+    end
+end
+
+ARC9.LoadAttachment(ATT, "eft_ammo_76239_apiz")
